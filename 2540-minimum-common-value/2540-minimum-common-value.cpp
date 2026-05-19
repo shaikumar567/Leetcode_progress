9d7ccc2afb84484d26c686f18ed7529a43ea995e
SHAIK MAHAMMAD UMAR <ms3411@srmist.edu.in>
2026-05-19 23:57:10 +0530

    Time: 0 ms (100%), Space: 54.4 MB (97.72%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+
+        int i = 0;
+        int j = 0;
+
+        while (i < nums1.size() && j < nums2.size()) {
+
+            int a = nums1[i];
+            int b = nums2[j];
+
+            if (a == b) {
+                return a;
+            }
+
+            if (a < b) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return -1;
+    }
+};
