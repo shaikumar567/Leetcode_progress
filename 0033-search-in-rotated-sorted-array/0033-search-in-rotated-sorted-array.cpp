c5ca57118130e237fe6d423bcbea04150a895e3b
SHAIK MAHAMMAD UMAR <ms3411@srmist.edu.in>
2026-05-22 22:36:19 +0530

    Time: 0 ms (100%), Space: 15.2 MB (66.43%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int curr = nums[mid];
+
+            if (curr == target) {
+                return mid;
+            }
+
+            if (nums[left] <= curr) {
+                if (target >= nums[left] && target < curr) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (target > curr && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
